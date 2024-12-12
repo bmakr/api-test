6aeefe04d817bc9ccf87a8fced86266f7ecd6d04
B Mack <lumeo@me.com>
2024-12-12 16:43:28 -0500

    add requireAuth

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(request: NextRequest) {
+export function requireAuthKey({ req }: { req: NextRequest }) {
+  // Get the ACTUALED_AUTH_KEY from environment variables
+  const expectedAuthKey = process.env.ACTUALED_AUTH_KEY
+  console.log({ expectedAuthKey})
+
+  // Check for the Authorization header
+  const authHeader = req.headers.get('Authorization');
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new Error('Unauthorized: 1')
+  }
+
+  // Extract the token from the header
+  const token = authHeader.split(' ')[1]
+  console.log({ token })
+  if (token !== expectedAuthKey) {
+    throw new Error('Unauthorized: 2')
+  }
+} 
+
+export async function POST(req: NextRequest) {
+  await requireAuthKey({ req })
+  const body = await req.json()
   return NextResponse.json({ message: 'Hello, world!' }, { status : 200 })
 }
